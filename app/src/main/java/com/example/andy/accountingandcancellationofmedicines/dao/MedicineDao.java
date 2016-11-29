0ddb491a6101561b0209c2bd5091de742747026d
Andy <andrian.belevitch@gmail.com>
2016-11-29 11:44:51 +0300

    All list medicine Dao

--- app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/MedicineDao.java
+++ app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/MedicineDao.java
@@ -1,8 +1,15 @@
 package com.example.andy.accountingandcancellationofmedicines.dao;
 
+import com.example.andy.accountingandcancellationofmedicines.entity.MedicineEntity;
+
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by Andy on 28.11.16.
  */
 
-public interface MedicineDao {
+public interface MedicineDao extends Serializable {
+
+    public List<MedicineEntity> queryAllMedicine() throws Exception;
 }
--- app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/MedicineDaoImpl.java
+++ app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/MedicineDaoImpl.java
@@ -1,8 +1,46 @@
 package com.example.andy.accountingandcancellationofmedicines.dao;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.andy.accountingandcancellationofmedicines.database.MedicineTable;
+import com.example.andy.accountingandcancellationofmedicines.database.Singl;
+import com.example.andy.accountingandcancellationofmedicines.entity.MedicineEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Andy on 28.11.16.
  */
 
-public class MedicineDaoImpl {
+public class MedicineDaoImpl implements MedicineDao {
+
+    private final SQLiteDatabase db;
+    private static final String LOG_TAG = MedicineDaoImpl.class.getName();
+
+    public MedicineDaoImpl(){
+        db = Singl.getInstance();
+    }
+    @Override
+    public List<MedicineEntity> queryAllMedicine() throws Exception {
+        Cursor c = db.query(MedicineTable.NameMedicineTable, null, null, null, null, null, null);
+        ArrayList<MedicineEntity> list = new ArrayList<>();
+
+        while(c.moveToNext()){
+            MedicineEntity medicineEntity = new MedicineEntity();
+            medicineEntity.setId(c.getInt(c.getColumnIndex(MedicineTable.ColumnMedicineTable.ID)));
+            medicineEntity.setNameMedicine(c.getString(c.getColumnIndex(MedicineTable.ColumnMedicineTable.NameMedicine)));
+            medicineEntity.setLotNumber(c.getInt(c.getColumnIndex(MedicineTable.ColumnMedicineTable.LotNumber)));
+            medicineEntity.setNote(c.getString(c.getColumnIndex(MedicineTable.ColumnMedicineTable.Note)));
+            medicineEntity.setAmount(c.getInt(c.getColumnIndex(MedicineTable.ColumnMedicineTable.Amount)));
+            medicineEntity.setArrivalDate(c.getString(c.getColumnIndex(MedicineTable.ColumnMedicineTable.ArrivalDate)));
+            medicineEntity.setDateOfManufacture(c.getString(c.getColumnIndex(MedicineTable.ColumnMedicineTable.DateOfManufacture)));
+            medicineEntity.setShelfLife(c.getString(c.getColumnIndex(MedicineTable.ColumnMedicineTable.ShelfLife)));
+
+            list.add(medicineEntity);
+        }
+        c.close();
+        return list;
+    }
 }
