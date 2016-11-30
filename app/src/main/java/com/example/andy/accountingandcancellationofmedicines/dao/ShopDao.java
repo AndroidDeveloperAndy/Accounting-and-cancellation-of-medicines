321bb895aa49b2de902879dc7b6955028af26901
Andy <andrian.belevitch@gmail.com>
2016-11-30 20:01:56 +0300

    dao for Shop(query all Shop)

--- app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/ShopDao.java
+++ app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/ShopDao.java
@@ -1,8 +1,15 @@
 package com.example.andy.accountingandcancellationofmedicines.dao;
 
+import com.example.andy.accountingandcancellationofmedicines.entity.ShopEntity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  * Created by Andy on 28.11.16.
  */
 
-public interface ShopDao {
+public interface ShopDao extends Serializable{
+
+    public ArrayList<ShopEntity> queryAllShop() throws Exception;
 }
--- app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/ShopDaoImpl.java
+++ app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/ShopDaoImpl.java
@@ -1,8 +1,44 @@
 package com.example.andy.accountingandcancellationofmedicines.dao;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.andy.accountingandcancellationofmedicines.database.ShopTable;
+import com.example.andy.accountingandcancellationofmedicines.database.Singl;
+import com.example.andy.accountingandcancellationofmedicines.entity.ShopEntity;
+
+import java.util.ArrayList;
+
 /**
  * Created by Andy on 28.11.16.
  */
 
-public class ShopDaoImpl {
+public class ShopDaoImpl implements ShopDao{
+
+    private final SQLiteDatabase db;
+    private static final String LOG_TAG = ShopDaoImpl.class.getName();
+
+    public ShopDaoImpl(){
+        db = Singl.getInstance();
+    }
+
+    @Override
+    public ArrayList<ShopEntity> queryAllShop() throws Exception {
+        Cursor c = db.query(ShopTable.NameShopTable, null, null, null, null, null, null);
+        ArrayList<ShopEntity> list = new ArrayList<>();
+
+        while(c.moveToNext()){
+            ShopEntity shopEntity = new ShopEntity();
+            shopEntity.setIdShop(c.getInt(c.getColumnIndex(ShopTable.ColumnShopTable.IdShop)));
+            shopEntity.setNameShop(c.getString(c.getColumnIndex(ShopTable.ColumnShopTable.NameShop)));
+            shopEntity.setAdressShop(c.getString(c.getColumnIndex(ShopTable.ColumnShopTable.AdressShop)));
+            shopEntity.setNameMedicineAtShop(c.getString(c.getColumnIndex(ShopTable.ColumnShopTable.NameMedicineAtShop)));
+            shopEntity.setPriceAtShop(c.getInt(c.getColumnIndex(ShopTable.ColumnShopTable.PriceAtShop)));
+
+            list.add(shopEntity);
+        }
+        c.close();
+        return list;
+    }
 }
+
