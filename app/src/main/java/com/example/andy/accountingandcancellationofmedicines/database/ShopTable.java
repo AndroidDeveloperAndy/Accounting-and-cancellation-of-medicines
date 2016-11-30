3fedfa5536465e5083b3b7df7fb88f66add72482
Andy <andrian.belevitch@gmail.com>
2016-11-30 20:02:45 +0300

    update table Shop

--- app/src/main/java/com/example/andy/accountingandcancellationofmedicines/database/ShopTable.java
+++ app/src/main/java/com/example/andy/accountingandcancellationofmedicines/database/ShopTable.java
@@ -6,13 +6,13 @@ package com.example.andy.accountingandcancellationofmedicines.database;
 
 public class ShopTable {
 
-    private static final String NameShopTable= "shop";
+    public static final String NameShopTable= "shop";
 
     public static final class ColumnShopTable{
-        private static final String NameMedicineAtShop= "NameMedicineAtShop";
-        private static final String IdShop= "IdShop";
-        private static final String NameShop= "NameShop";
-        private static final String AdressShop= "AdressShop";
-        private static final String PriceAtShop= "PriceAtShop";
+        public static final String NameMedicineAtShop= "NameMedicineAtShop";
+        public static final String IdShop= "IdShop";
+        public static final String NameShop= "NameShop";
+        public static final String AdressShop= "AdressShop";
+        public static final String PriceAtShop= "PriceAtShop";
     }
 }
