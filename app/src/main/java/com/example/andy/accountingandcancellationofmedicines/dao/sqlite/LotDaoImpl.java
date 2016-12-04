4ccf5db9714a2a5e4a3cfc7246a4d1cc3745cfd0
Andy <andrian.belevitch@gmail.com>
2016-12-04 17:12:13 +0300

    add query Lot Number

--- app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/sqlite/LotDaoImpl.java
+++ app/src/main/java/com/example/andy/accountingandcancellationofmedicines/dao/sqlite/LotDaoImpl.java
@@ -1,8 +1,46 @@
 package com.example.andy.accountingandcancellationofmedicines.dao.sqlite;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.andy.accountingandcancellationofmedicines.dao.LotDao;
+import com.example.andy.accountingandcancellationofmedicines.database.LotTable;
+import com.example.andy.accountingandcancellationofmedicines.database.Singl;
+import com.example.andy.accountingandcancellationofmedicines.entity.LotEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Andy on 28.11.16.
  */
 
-public class LotDaoImpl {
+public class LotDaoImpl implements LotDao{
+
+    private final SQLiteDatabase db;
+
+    public LotDaoImpl() {
+        db = Singl.getInstance();
+    }
+
+    @Override
+    public List<LotEntity> queryLot() throws Exception {
+        Cursor c = db.query(LotTable.NameLotTable, null, null, null, null, null, null);
+        ArrayList<LotEntity> list = new ArrayList<>();
+
+        while(c.moveToNext()){
+            LotEntity lotEntity = new LotEntity();
+            lotEntity.setIdLot(c.getInt(c.getColumnIndex(LotTable.ColumnLotTable.IdLot)));
+            lotEntity.setlNumber(c.getInt(c.getColumnIndex(LotTable.ColumnLotTable.LNumber)));
+            lotEntity.setAdress(c.getString(c.getColumnIndex(LotTable.ColumnLotTable.Adress)));
+            lotEntity.setDepartureDate(c.getString(c.getColumnIndex(LotTable.ColumnLotTable.DepartureDate)));
+            lotEntity.setNameCity(c.getString(c.getColumnIndex(LotTable.ColumnLotTable.NameCity)));
+            lotEntity.setNameCountry(c.getString(c.getColumnIndex(LotTable.ColumnLotTable.NameCountry)));
+            lotEntity.setPriceOne(c.getInt(c.getColumnIndex(LotTable.ColumnLotTable.PriceOne)));
+
+            list.add(lotEntity);
+        }
+        c.close();
+        return list;
+    }
 }
